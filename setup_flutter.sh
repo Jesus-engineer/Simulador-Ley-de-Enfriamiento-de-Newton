8759c6611dd3f24bc6b130257edaee1d37376abc
yosser <yosserjulio2@gmail.com>
2025-11-04 23:01:12 -0500

    solucion despli

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Instalar Flutter y compilar la app para web
 
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
-tar xf flutter_linux_3.24.3-stable.tar.xz
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.4-stable.tar.xz
+tar xf flutter_linux_3.24.4-stable.tar.xz
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# 🔧 Solución al error de "dubious ownership"
+git config --global --add safe.directory /vercel/path0/flutter
+
 flutter config --enable-web
 flutter build web --release
