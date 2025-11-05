7d134f0fdc8e5a2978968fad8766373ca16eec2f
yosser <yosserjulio2@gmail.com>
2025-11-04 23:03:51 -0500

    mejorando el despliegue en Vercel

--- setup_flutter.sh
+++ setup_flutter.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Instalar Flutter y compilar la app para web
 
+set -e  # Hace que el script se detenga si hay cualquier error
+
+# Descargar Flutter (versión estable compatible con fl_chart 1.1.1)
 curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.4-stable.tar.xz
 tar xf flutter_linux_3.24.4-stable.tar.xz
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 🔧 Solución al error de "dubious ownership"
+# Solución al error de "dubious ownership"
 git config --global --add safe.directory /vercel/path0/flutter
 
 flutter config --enable-web
